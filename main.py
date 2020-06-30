4598db9abffadb682a28c90f2adb27d8991d851c
Santiago <santiagoperaza@icloud.com>
2020-06-30 14:09:09 -0300

    init main

---
+++ main.py
@@ -0,0 +1,10 @@
+# Biomecanica trabajo final
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+file = 'data_proyecto_biomec2020.npy'
+data = np.load(file).items()
+
+# data2 = pd.DataFrame(file)
